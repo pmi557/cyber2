d433e7cba84b9c73f18bbb46c2cd35bc2a2a0e1e
pmi557 <pmujta@gmail.com>
2018-02-23 10:30:33 +0100

    Initial version

---
+++ Readme.md
@@ -0,0 +1,2 @@
+#Uruchamianie programu z konsoli
+java -cp target/cyber2-1.0-SNAPSHOT.jar com.pmi.app.App
---
+++ src/main/java/com/pmi/app/App.java
@@ -0,0 +1,14 @@
+package com.pmi.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+        }
+}
