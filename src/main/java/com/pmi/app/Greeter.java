2a5c61dd1f1c110043e396c9768749aab3da0d2b
pmi557 <pmujta@gmail.com>
2018-02-23 12:01:25 +0100

    ok

--- src/main/java/com/pmi/app/Greeter.java
+++ src/main/java/com/pmi/app/Greeter.java
@@ -2,6 +2,6 @@ package com.pmi.app;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world!";
+    return "Hello world! I'm just testing java for understand the progs :)";
   }
 }
